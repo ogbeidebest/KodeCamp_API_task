78073e8c0680ee5b1a8b5bb6759da288d28269b8
ogbeidebest <ogbeidebest.ob@gmail.com>
2021-11-02 14:46:34 +0100

    hmmm

--- js/app.js
+++ js/app.js
@@ -10,10 +10,10 @@ function getPosts() {
             posts.forEach(e => {
 
                 html += `
-                <div class="col-md-4 col-lg-4 col-12 h-100 mb-3" id="">
+                <div class="col-12 col-md-4 col-lg-4 mb-4" id="">
                      <a class="text-decoration-none text-dark"  href='singlepost.html' onclick="sadCard(${e.id})">
-                        <div class="card h-100" id="${e.id}" >     
-                            <div class="card-body h-100" id="${e.id}" >
+                        <div class="card h-100 mb-4 border border-dark" id="${e.id}" >     
+                            <div class="card-body id="${e.id}" >
                                      <h2 class="d-flex justify-content-center text-danger" >${e.id}</h2>
                                      <h5 class="post-title mb-4 text-center">${e.title}</h5>
                                     <p class=" post-body text-justify">${e.body}</p>
@@ -31,13 +31,8 @@ function getPosts() {
 
         })
 }
-
+var posts = localStorage.setItem("post", JSON.stringify(posts));
 
 
 getPosts();
-
-
-function sadCard(num) {
-    localStorage.setItem('postId', num);
-
-}
+posts = localStorage.setItem("post", num);
